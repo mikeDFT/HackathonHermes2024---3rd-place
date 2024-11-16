cd4afcd45f1865f2e57b3d2ae0d32b37852cb0a3
DrNoris <78544459+DrNoris@users.noreply.github.com>
2024-11-16 15:37:52 +0200

    Add files via upload

---
+++ Code/Domain/Platform.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Platform:
+    def __init__(self, screen, x, y, width=75, height=5, color=(252, 186, 3)):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.width = width  # Allows different platform widths
+        self.height = height  # Allows different platform heights
+        self.color = color  # Customizable color
+
+    def render(self):
+        """Render the platform on the screen."""
+        pygame.draw.rect(self.screen, self.color, (self.x, self.y, self.width, self.height))
+
+    def is_colliding(self, player_rect):
+        """
+        Check if the platform is colliding with a given player's rect.
+        The player_rect should be a pygame.Rect object.
+        """
+        platform_rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        return platform_rect.colliderect(player_rect)
